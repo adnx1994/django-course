ab8f878a4e1a430a688dc7f90db314d549dea124
Alireza <169320894+adnx1994@users.noreply.github.com>
2024-08-11 17:07:29 +0330

    form contact us

--- cortex_with_form_contactus/cotrex/contact_module/views.py
+++ cortex_with_form_contactus/cotrex/contact_module/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -9,4 +10,5 @@ def contact(request):
         print(request.POST)
         print(request.POST['username'])
         print(request.POST['email'])
+        return redirect(reverse('main'))
     return render(request,"contact_module/contact_page.html")
